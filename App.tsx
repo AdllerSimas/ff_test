10887f7ceed8b2ee33b9026e356eec7ab72d5475
Adller Simas <adller.eel.ufsc@gmail.com>
2020-09-01 15:15:49 -0300

    splash screen with react native

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const App: React.FC = () => {
+  
+  useEffect(() => {
+    // verificações anteriores
+    SplashScreen.hide();
+  }, []);
+
+  
+  
+  
+  return (
+    <View>
+      <Text style={{color: '#000000'}}> It's just a test!</Text>
+    </View>
+  )
+}
+
+export default App
